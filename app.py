4e3e2facc7731515661520c647c1472b72100c66
duds0901 <duda.mobal@gmail.com>
2024-05-03 12:13:07 -0300

    subir mudancas

--- app.py
+++ app.py
@@ -1,95 +1,125 @@
 
-import os 
-#inserir 2 restaurantes na list
-# restaurantes=['Bife Sujo', 'Saco de Feijão'] 
-restaurantes=[{'nome':'Bife Sujo','categoria':'prato-feito','ativo':True},
-              {'nome':'Saco de Feijão','categoria':'feijoada', 'ativo':False},
-              {'nome':'Pé de Banha','categoria':'pastelaria','ativo':True}]
+#1 10/04 imoport lib os (operational system)
+import os
 
 
-def mostrar_subtitulo():
-    os.system('cls')
-    print ('texto')
+#restaurantes=['Bife Sujo', 'Saco de Feijão']
+#inserir dicionario-em outras linguagens chave valor
+restaurantes=[{'nome':'Bife-sujo','categoria':'prato-feito','ativo': True},
+              {'nome':'Saco do feijão','categoria':'feijoada','ativo': False},
+               {'nome':'O bola 8 de coco','categoria':'doceria','ativo': True},] 
+
+def mostrar_subtitulo(texto):
+    os.system('clear')
+    print (texto)
     print()
-    
+
+#2 declarando a função finalizar_app
 def finalizar_app():
-    # os.system('cls')
-    # print ('Finalizando app\n')
-    mostrar_subtitulo('Finalizando o app')
-
-
+    
+    mostrar_subtitulo()
 
 def chamar_nome_do_app():
-     print ('''𝓻𝓮𝓼𝓽𝓪𝓾𝓻𝓪𝓷𝓽𝓮 𝓮𝔁𝓹𝓻𝓮𝓼𝓼𝓸''') 
-
-def opcao_invalida():
-    print('Opção invalida\n')
-    # input('Digite uma tecla para voltar ao menu principal: ')
-    # main() 
-    voltar_ao_menu_principal()
-
-def exibir_opcoes():
-    print('1-Cadastrar um restaurante')
-    print('2-Listar restaurantes')
-    print('3-Ativar um restaurante')
-    print('4-Sair do programa')
+    print ('''
+    
+    ℜ𝔢𝔰𝔱𝔞𝔲𝔯𝔞𝔫𝔱𝔢 𝔈𝔵𝔭𝔯𝔢𝔰𝔰𝔬
+    
+    ''')
 
 def voltar_ao_menu_principal():
-     input("\n Digite uma tecla para voltar ao menu principal: \n")
-     main() 
+     input('\nDigite uma tecla para voltar ao menu principal')
+     main()
+
+# 12 criando opção_invalida
+def opcao_invalida():
+    print ('opção invalida\n')
+    #input('Digite uma tecla para voltar ao menu principal:')
+    #main()
+    voltar_ao_menu_principal()
+    
+def exibir_opcoes():
+    print ("1 Cadastrar Restaurante")
+    print ("2 Listar Restaurante")
+    print ("3 Ativar Restaurente")
+    print ("4 Sair\n")
 
 
-def cadastrar_novo_restaurante():
-     os.system('cls')
-     nome_do_restaurante=input('Digite o nome do novo restaurante: \n')
-     restaurantes.append(nome_do_restaurante)
-     print(f'O restaurante {nome_do_restaurante}, foi cadastrado com sucesso\n')
-     voltar_ao_menu_principal()
+def cadatrar_novo_restaurante():
+    os.system('clear')
+    nome_do_restaurante= input('digite o nome do novo restaurante:')
+    categoria=input(f'Digite a categoria do restaurante {nome_do_restaurante}:')
+    dados_do_restaurante={'nome':nome_do_restaurante, 'categoria':categoria, 'ativo': False}
+    restaurantes.append(dados_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante}, foi cadastrado com sucesso\n')
+    voltar_ao_menu_principal()
 
 def listar_restaurantes():
-    os. system('cls')
-    print('Listando os restaurantes \n')
-    #em português
+    mostrar_subtitulo('Listando os restaurantes')
     for restaurante in restaurantes:
-        # print(f'-{restaurante}')
-    #modificando a maneira de listar restaurante
-    #para manipular o dicionário
-        nome_res=restaurante['nome']
-        categoria=restaurante['categoria']
-        print(f'-{nome_res} |- {categoria}')
-        voltar_ao_menu_principal()
+       # print(f'- {restaurantes}')
+       #modificando a maneira de listar dicionario
+       nome_restaurante=restaurante['nome']
+       categoria=restaurante['categoria']
+       ativo=restaurante ['ativo']
+       print(f'-{nome_restaurante} | -{categoria} | -{ativo}')
+    #chamar a duas funções e saída
+    voltar_ao_menu_principal()
 
 
+def alterar_estado_restaurante():
+    mostrar_subtitulo('Alternando o estado do restaurante')
+    nome_restaurante=input('Digite o nome do restaurante que desejas alternar o estado: ')
+    restaurante_encontrado=False
 
-def escolher_opcoes(): 
+    for restaurante in restaurantes:
+        if nome_restaurante==restaurante['nome']:
+            restaurante_encontrado=True
+            restaurante['ativo']=not restaurante['ativo']
+            mensagem=f'O restaurante {nome_restaurante} foi ativado com sucesso'if restaurante['ativo'] else f'Restaurante {nome_restaurante} foi desativado com sucesso!'
+            print(mensagem)
+    if not restaurante_encontrado:
+        print('O restaurante não foi encontrado')        
+    voltar_ao_menu_principal()
+
+
+#8 declarando a função opcao_digitada1
+def escolher_opcao():
+    #11 adcionando o try
     try:
-        opcao_digitada=(int(input('Escolha uma opção: ')))
-        print('Você selecionou a opção: ',opcao_digitada,'\n')
+        opcao_digitada = (int(input("Escolha uma opção")))
+        print ("Você selecionou:",opcao_digitada, "\n")
         if(opcao_digitada==1):
-            print('Você escolheu cadastrar um restaurante\n')
-            cadastrar_novo_restaurante()
-        elif (opcao_digitada==2):
-            # print('Você escolheu listar os restaurantes do app\n')
-            listar_restaurantes()
-        elif (opcao_digitada==3):
-            print('Você escolheu ativar um restaurante\n')
-        elif (opcao_digitada==4):
-            print('Você escolheu sair do programa\n')
+            print("Você escolheu Cadastrar Restaurante\n")
+            cadatrar_novo_restaurante()
             finalizar_app()
+        elif(opcao_digitada==2):
+           # print("Você escolheu Listar Restaurante\n") 
+           listar_restaurantes()
+           finalizar_app()
+        elif(opcao_digitada==3):
+            #mostrar_subtitulo("Voce escolheu Ativar Restaurante")
+            alterar_estado_restaurante()
+            finalizar_app()
+        elif(opcao_digitada==4):
+            # print('Voce escolheu sair do programa') 
+             finalizar_app()
+        #3 chamando a função finalizar_app 
         else:
             opcao_invalida()
     except:
-        opcao_invalida() 
-
+        opcao_invalida()         
+  
+  #5 escrever a funçaõ main
 def main():
-    os.system('cls') 
+    #10 clear
+    os.system('clear')
+    #6 chamar o nome do app
+    chamar_nome_do_app()
+    #7 chamar a escolha de opções
+    exibir_opcoes()
+    #9 chamar a opção digitada
+    escolher_opcao()
 
-    chamar_nome_do_app() 
-
-    exibir_opcoes() 
-
-    escolher_opcoes() 
-
-
+#4 criando a entrada através da variável main
 if(__name__=='__main__'):
     main()
